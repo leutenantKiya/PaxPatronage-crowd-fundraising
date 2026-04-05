6977b3cef5a05e213649172ca283e09938cd359a
leutenantKiya <antoniuskiya57@gmail.com>
2026-04-05 15:44:07 +0700

    adding some script

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+var navbar_responsive = document.getElementById("top");
+
 document.querySelectorAll('.progress-fill').forEach(bar => {
     const target = bar.style.width;
     bar.style.width = '0';
@@ -5,3 +7,11 @@ document.querySelectorAll('.progress-fill').forEach(bar => {
       requestAnimationFrame(() => { bar.style.width = target; });
     });
 });
+
+function responsive_navbar(){
+  if (navbar_responsive.className === "top"){
+    navbar_responsive.className += " responsive";
+  }else{
+    navbar_responsive.className += "top";
+  }
+}
